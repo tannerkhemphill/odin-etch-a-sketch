e8c26144f6bcc853a9d7c0e35874f9a9f29f09af
Tanner Hemphill <tannerkhemphill@gmail.com>
2021-12-02 07:59:16 -0500

    Add function to make grid

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const container = document.createElement('div');
-container.setAttribute('id', 'container');
-for (let i = 0; i < 16; i++) {
-    let div = document.createElement('div');
-    div.classList.toggle('grid');
-    container.appendChild(div);
-}
+const container = document.getElementById('container');
+
+function makeGrid(num) { 
+    for (let i = 0; i < num*num; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('grid');
+        container.appendChild(cell);
+    };
+    container.style.setProperty("grid-template-columns", `repeat(${num}, 2fr)`);
+    container.style.setProperty("grid-template-rows", `repeat(${num}, 2fr)`);
+};
+
+makeGrid(16);
