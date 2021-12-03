0d4d7d28a04bf2d8810a537896663393e976928c
Tanner Hemphill <tannerkhemphill@gmail.com>
2021-12-03 01:06:01 -0500

    Add draw and reset button functionality

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ function makeGrid(num) {
     };
     container.style.setProperty("grid-template-columns", `repeat(${num}, 2fr)`);
     container.style.setProperty("grid-template-rows", `repeat(${num}, 2fr)`);
+
+    let grid = document.querySelectorAll('.grid');
+
+    grid.forEach(cell => cell.addEventListener("mouseenter", function(e) {
+    e.target.style.backgroundColor = "black";
+}));
+};
+
+function reset() {
+    document.querySelectorAll(".grid").forEach((e) => e.parentNode.removeChild(e));
+    let num = -1;
+    while (isNaN(num) || num < 1 || num > 100) {
+        num = parseInt(window.prompt("Please enter a number from 1 to 100", ""), 10);
+    };
+    makeGrid(num);
 };
 
 makeGrid(16);
+
+const button = document.getElementById('btn');
+
+button.onclick = () => reset();
